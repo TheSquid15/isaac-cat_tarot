cd3ddae4eb8da40a81bf4c0d116e4f2367db26f3
TheSquid15 <viv@energycluster.dk>
2025-08-07 22:50:10 +0200

    added a print statement to the on use card to see if the function is being called

--- main.lua
+++ main.lua
@@ -1,15 +1,17 @@
 local CatTarot = RegisterMod("CatTarot", 1)
 
 function CatTarot:OnUseCard(card, player, flags)
-    local skipCards = {
+    --[[ local skipCards = {
         [Card.CARD_CHAOS] = true,
         [Card.CARD_CRACKED_KEY] = true,
         [Card.CARD_AFFINITY] = true,
-    }
+    } ]]
 
-    if not skipCards[card] then
-        Isaac.ShowGiantBook("The " .. Isaac.GetCardName(card), "", nil)
-    end
+    --[[ if not skipCards[card] then
+        Isaac.ShowGiantBook(Isaac.GetCardName(card), "", "giantbook")
+    end ]]
+
+    print("Hello Isaac!")
 end
 
 CatTarot:AddCallback(ModCallbacks.MC_USE_CARD, CatTarot.OnUseCard)
