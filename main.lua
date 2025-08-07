7861669ace7339c40ac8767de2cbacb4c7875f23
TheSquid15 <viv@energycluster.dk>
2025-08-07 22:50:36 +0200

    Removed some of the old code that didnt work anyway

--- main.lua
+++ main.lua
@@ -1,16 +1,6 @@
 local CatTarot = RegisterMod("CatTarot", 1)
 
 function CatTarot:OnUseCard(card, player, flags)
-    --[[ local skipCards = {
-        [Card.CARD_CHAOS] = true,
-        [Card.CARD_CRACKED_KEY] = true,
-        [Card.CARD_AFFINITY] = true,
-    } ]]
-
-    --[[ if not skipCards[card] then
-        Isaac.ShowGiantBook(Isaac.GetCardName(card), "", "giantbook")
-    end ]]
-
     print("Hello Isaac!")
 end
 
